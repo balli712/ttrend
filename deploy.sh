58b42c618315bac94f98cb942cc5affc300890cd
balli712 <98575767+balli712@users.noreply.github.com>
2024-09-03 20:00:01 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-kubectl apply -f namespace.yml
+kubectl apply -f namespace.yaml
 kubectl apply -f secret.yaml
 kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f services.yaml
 echo 'finish'
